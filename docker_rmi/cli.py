223ef933d67786f5e2ed0da1bb61c22c467ac1af
Khoa Nguyen <vankhoa011@gmail.com>
2015-11-27 16:38:01 +0700

    Update handle exception when remove image

--- docker_rmi/cli.py
+++ docker_rmi/cli.py
@@ -2,6 +2,7 @@
 import click
 HAS_DOCKER_PY = False
 try:
+    import docker
     from docker import Client
     from docker.utils import kwargs_from_env
     c = Client(version='auto',**kwargs_from_env(assert_hostname=False) )
@@ -16,12 +17,23 @@ def remove_image(repo, number_version):
         images = c.images(filters={"dangling":"true"}, quiet="true")
         if images:
             for image in images:
-                c.remove_image(image=image)
+                try:
+                    c.remove_image(image=image)
+                except docker.errors.APIError as de:
+                    print de
+                except Exception as ex:
+                    print ex
+
     images = c.images(name=repo)
 
     if len(images) >= number_version:
         for i in range(number_version, len(images)):
-            c.remove_image(image=images[i]['Id'])
+            try:
+                c.remove_image(image=images[i]['Id'])
+            except docker.errors.APIError as de:
+                print de
+            except Exception as ex:
+                print ex
 
 
 def get_list_repo(repo, number_version):
