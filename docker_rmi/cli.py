60f79b68642dcacbbc378459f80b01adbfc829e9
Khoa Nguyen <vankhoa011@gmail.com>
2015-11-30 20:49:20 +0700

    Update cli follow pep8 standard

--- docker_rmi/cli.py
+++ docker_rmi/cli.py
@@ -1,11 +1,11 @@
- #! /usr/bin/env python
+#!/usr/bin/env python
 import click
 HAS_DOCKER_PY = False
 try:
     import docker
     from docker import Client
     from docker.utils import kwargs_from_env
-    c = Client(version='auto',**kwargs_from_env(assert_hostname=False) )
+    c = Client(version='auto', **kwargs_from_env(assert_hostname=False))
     HAS_DOCKER_PY = True
 except ImportError:
     HAS_DOCKER_PY = False
@@ -14,7 +14,7 @@ except ImportError:
 def remove_image(repo, number_version):
     """ Remove image """
     if repo == '<none>':
-        images = c.images(filters={"dangling":"true"}, quiet="true")
+        images = c.images(filters={"dangling": "true"}, quiet="true")
         if images:
             for image in images:
                 try:
@@ -39,28 +39,26 @@ def remove_image(repo, number_version):
 def get_list_repo(repo, number_version):
     """ Get list repo """
 
-    if repo == 'all' :
+    if repo == 'all':
         images = c.images()
         repos = []
         for image in images:
             repos.append(image['RepoTags'][0].split(":")[0])
-
         repos = set(repos)
         for r in repos:
-            remove_image( r , number_version)
+            remove_image(r, number_version)
     else:
-        remove_image( repo, number_version)
+        remove_image(repo, number_version)
 
 
 @click.command()
-@click.option('--repo', '-r' , default='all', help='Specific one image repo or all repo' )
+@click.option('--repo', '-r', default='all', help='Specific one image repo or all repo')
 @click.option('--number-version', '-n', default=3, help='Number version want to keep')
-def main( repo, number_version):
+def main(repo, number_version):
     """Docker Remove Obsolete Image"""
-    if HAS_DOCKER_PY == False :
+    if HAS_DOCKER_PY is not True:
         print "docker-py is required"
     get_list_repo(repo, number_version)
 
 if __name__ == '__main__':
     main()
-
